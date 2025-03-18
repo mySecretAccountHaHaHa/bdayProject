9fbc2aa8e323e7f6c89857ddf86a765d928bd761
Secret <nevermindimanidiot@gmail.com>
2025-03-18 13:04:58 +0530

    testing2

--- src/components/FinalShow.tsx
+++ src/components/FinalShow.tsx
@@ -1,7 +1,7 @@
 export default function FinalShow() {
     return <div className="flex flex-col items-center justify-center">
         <div className="p-5 font-delius text-8xl font-black text-pink-600">Happy Birthday Meri Jaan</div>
-        <img src="../src/assets/bd/blow.png" alt="budday" className="p-5" />
+        <img src="/assets/bd/blow.png" alt="budday" className="p-5" />
         <div className="font-delius text-3xl p-25 pt-5 flex items-center justify-center">
             20th Birthday. Mera pyaara bachha is a big girl now. Happy birthday meri jaan. May this year be the best of all. You'll achieve everything you want, just be patient.
             And don't panic so easily buddhu. I love love you. Happy birthday bachha❤️
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ import FinalShow from "../components/FinalShow";
 export default function Home() {
     const [stage, setStage] = useState(0);
 
-    const audioRef = useRef(new Audio("./src/assets/Baar Baar.mp3"));
+    const audioRef = useRef(new Audio("/assets/Baar Baar.mp3"));
 
     return (
         (stage < 4 && <div className='flex flex-col items-center justify-center bg-[#c6daec]'>
@@ -15,7 +15,7 @@ export default function Home() {
             {stage == 1 && <div className="font-emblema text-2xl">There's more to come</div>}
         </div>
         
-        <Card base={"./src/assets/her/" + ((stage == 0 && "welcome.jpg") || (stage == 1 && "cake.jpg") || (stage == 2 && "cap2.jpg") || (stage == 3 && "eat.jpg"))} cake={"./src/assets/cake.png"} slice={"./src/assets/slice.png"} cap={"./src/assets/cap.png"} stage={stage}/>
+        <Card base={"/assets/her/" + ((stage == 0 && "welcome.jpg") || (stage == 1 && "cake.jpg") || (stage == 2 && "cap2.jpg") || (stage == 3 && "eat.jpg"))} cake={"/assets/cake.png"} slice={"/assets/slice.png"} cap={"/assets/cap.png"} stage={stage}/>
 
         <button type="button" className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" onClick={() => {
             setStage(x => x + 1)
